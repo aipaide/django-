aaa01482b1f483bc86f7cba29d631e9a5016cd7a
jiangwr <jiangweirong10@mail.163.com>
2018-12-19 21:50:57 +0800

    add description for filter query params

--- people/views.py
+++ people/views.py
@@ -1,11 +1,39 @@
 from rest_framework import viewsets
-import django_filters.rest_framework
+import django_filters.rest_framework as filters
+import django_filters
+import rest_framework.permissions as permissions
+from rest_framework.pagination import LimitOffsetPagination
+
+from drf_yasg import openapi
+from drf_yasg.app_settings import swagger_settings
+from drf_yasg.inspectors import CoreAPICompatInspector, FieldInspector, NotHandled, SwaggerAutoSchema
+from drf_yasg.utils import no_body, swagger_auto_schema
+
+from django.utils.decorators import method_decorator
 
 from .models import Identity, Person
 from .serializers import IdentitySerializer, PersonSerializer
 
-import rest_framework.permissions as permissions
-from rest_framework.pagination import LimitOffsetPagination
+class DjangoFilterDescriptionInspector(CoreAPICompatInspector):
+    def get_filter_parameters(self, filter_backend):
+        if isinstance(filter_backend, filters.DjangoFilterBackend):
+            result = super(DjangoFilterDescriptionInspector, self).get_filter_parameters(filter_backend)
+            for param in result:
+                if not param.get('description', ''):
+                    param.description = "Filter the returned list by {field_name}".format(field_name=param.name)
+
+            return result
+
+        return NotHandled
+
+class PersonFilter(django_filters.FilterSet):
+    person__first_name=filters.CharFilter(field_name="person__first_name",lookup_expr="exact")
+    person__second_name=filters.CharFilter(field_name="person__second_name",lookup_expr="exact")
+    class Meta:
+        model = Person
+        fields=["person__first_name","person__second_name"]
+
+
 class PersonViewSet(viewsets.ModelViewSet):
     """
     PersonViewSet class docstring
@@ -21,8 +49,9 @@ class PersonViewSet(viewsets.ModelViewSet):
     serializer_class = PersonSerializer
     permission_classes=[permissions.IsAuthenticated,]
     pagination_class = LimitOffsetPagination
-    filter_backends = [django_filters.rest_framework.DjangoFilterBackend,]
-    filter_fields = ('id',)
+    filter_backends = [filters.DjangoFilterBackend,]
+    #filter_fields = ('id',)
+    filterset_class =PersonFilter
 
     def create(self, request, *args, **kwargs):
         """
@@ -32,6 +61,7 @@ class PersonViewSet(viewsets.ModelViewSet):
         """
         return  super(PersonViewSet,self).create(request,args,kwargs)
 
+    @swagger_auto_schema(filter_inspectors=[DjangoFilterDescriptionInspector])
     def list(self, request, *args, **kwargs):
         return super(PersonViewSet,self).list(request,args,kwargs)
 
@@ -39,3 +69,5 @@ class IdentityViewSet(viewsets.ModelViewSet):
     model = Identity
     queryset = Identity.objects
     serializer_class = IdentitySerializer
+
+
