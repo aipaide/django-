b0f2d634b77288f65d37c4fd88c65472da46278f
jiangwr <jiangweirong10@mail.163.com>
2018-12-19 16:38:43 +0800

    viewset docstring

--- people/views.py
+++ people/views.py
@@ -6,11 +6,27 @@ from .serializers import IdentitySerializer, PersonSerializer
 import rest_framework.permissions as permissions
 
 class PersonViewSet(viewsets.ModelViewSet):
+    """
+    PersonViewSet class docstring
+
+    retrieve:
+    summary of person_retrieve
+    
+    descriptive body of person_retrieve
+
+    """
     model = Person
     queryset = Person.objects
     serializer_class = PersonSerializer
     permission_classes=[permissions.IsAuthenticated,]
 
+    def create(self, request, *args, **kwargs):
+        """
+        summary of person_create
+
+        descriptive body of person_create
+        """
+        super(PersonViewSet,self).create(request,args,kwargs)
 
 class IdentityViewSet(viewsets.ModelViewSet):
     model = Identity
