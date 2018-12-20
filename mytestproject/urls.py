f4c3fb58088c01500a59ec426652271ffe4d33b0
jiangwr <jiangweirong10@mail.163.com>
2018-12-20 11:10:54 +0800

    设置默认openapi.info

--- mytestproject/urls.py
+++ mytestproject/urls.py
@@ -19,20 +19,22 @@ from django.conf.urls import include, url
 
 # rest framework
 from rest_framework import permissions
+from rest_framework.documentation import include_docs_urls
 
 #drf schema_view
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+swagger_info=openapi.Info(
+    title="MyTesting API",
+    default_version='1.0.0',
+    description="这是一个测试API",
+    terms_of_service="https://www.google.com/policies/terms/",
+    contact=openapi.Contact(email="jiangweirong10@163.com"),
+    license=openapi.License(name="BSD License"),
+)
 schema_view = get_schema_view(
-   openapi.Info(
-      title="MyTesting API",
-      default_version='v1',
-      description="这是一个测试API",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="jiangweirong10@163.com"),
-      license=openapi.License(name="BSD License"),
-   ),
+   swagger_info,
    validators=['flex', 'ssv'],
    public=True,
    permission_classes=(permissions.AllowAny,),
@@ -49,9 +51,12 @@ urlpatterns = [
     # redoc ui
    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    # cached 
+    # cached
     url(r'^cached/swagger(?P<format>.json|.yaml)$', schema_view.without_ui(cache_timeout=None), name='cschema-json'),
     url(r'^cached/swagger/$', schema_view.with_ui('swagger', cache_timeout=None), name='cschema-swagger-ui'),
     url(r'^cached/redoc/$', schema_view.with_ui('redoc', cache_timeout=None), name='cschema-redoc'),
 
+    url(r'^internal/doc/', include_docs_urls(title='Your API',
+                                    authentication_classes=[],
+                                    permission_classes=[])),
 ]
